38b1cd9e5aefdb040563221790b6b644a841d3ba
kawika98 <kawikahodge@gmail.com>
2021-03-14 16:56:20 -0400

    Compleated holder_Information so we can now transfer info between saves
    Compleated holder_Information so we can now transfer info between saves

--- Watermelon Massacre/Assets/Scenes/Level 1/Scripts/GameManager.cs
+++ Watermelon Massacre/Assets/Scenes/Level 1/Scripts/GameManager.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
+    [Header("Saves")]
+    public holder_Information save1;
+    public holder_Information save2;
+    public holder_Information save3;
+
+    public holder_Information currentSave;
+
+
+
+
+    private void Awake()
+    {
+        loadSave();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +29,64 @@ public class GameManager : MonoBehaviour
     void Update()
     {
        if(PlayerManager.playerisDead == true)
+       {
+            playerDeath();
+       }
+    }
+
+
+
+    private void playerDeath()
+    {
+        int curSave = save1.currentSave;
+
+        if (curSave == 1)
         {
+            currentSave.save(save1);
+            goTo_mainMenu();
+        }
+        else if (curSave == 2)
+        {
+            currentSave.save(save2);
+            goTo_mainMenu();
 
         }
+        else if (curSave == 3)
+        {
+            currentSave.save(save3);
+            goTo_mainMenu();
+        }
+        else
+        {
+            Debug.LogError("currentSave is invalid check save1 for more details");
+        }
+    }
+
+    private void goTo_mainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);//0 should be main menu
+    }
+    
+    private void loadSave()
+    {
+        int curSave = save1.currentSave;
+
+        if (curSave == 1)
+        {
+            save1.save(currentSave);
+        }
+        else if (curSave == 2)
+        {
+            save1.save(currentSave);
+        }
+        else if (curSave == 3)
+        {
+            save1.save(currentSave);
+        }
+        else
+        {
+            Debug.LogError("currentSave is invalid check save1 for more details");
+        }
+
     }
 }
